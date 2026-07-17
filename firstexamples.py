e05b5d48d21ac49af57af06897e4b5a5bb20cad0
Kishore Kavati <kishoreklm44@gmail.com>
2026-07-17 07:57:44 +0530

    Completed Pandas examples

---
+++ firstexamples.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "id": [1, 2, 3],
+    "name": ["Kishore", "Ravi", "Suresh"],
+    "salary": [50000, 60000, 70000]
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv("employees.csv", index=False)
+
+print("CSV file created successfully")
